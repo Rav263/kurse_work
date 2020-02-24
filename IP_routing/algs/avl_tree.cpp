5778f038c44407522894a1941712f93eeee56491
Nikita Nikiforov <rav263@altlinux.org>
2020-02-25 01:08:38 +0300

    end of test version of avl tree

--- IP_routing/algs/avl_tree.cpp
+++ IP_routing/algs/avl_tree.cpp
@@ -7,35 +7,111 @@
 #include "../headers/table_io.h"
 #include "../headers/tree.h"
 
+//#define DEBUG
+
 bool prefix_compare(Net_IP net_ip_1, Net_IP net_ip_2) {
     if (net_ip_1.second == net_ip_2.second) {
+#ifdef DEBUG
         std::cout << intip_to_string(net_ip_1) << "(" << net_ip_1.first << ") < ";
         std::cout << intip_to_string(net_ip_2) << "(" << net_ip_2.first << ") = ";
+#endif
         return net_ip_1.first < net_ip_2.first;
     }
     if (net_ip_1.second < net_ip_2.second) { 
+#ifdef DEBUG
         std::cout << intip_to_string(net_ip_1) << "(" << (net_ip_1.first >> (32 - net_ip_1.second))<< ") < ";
         std::cout << intip_to_string(net_ip_2) << "(" << (net_ip_2.first >> (32 - net_ip_1.second))<< ") = "; 
+#endif
         return (net_ip_1.first >> (32 - net_ip_1.second)) < (net_ip_2.first >> (32 - net_ip_1.second));
     }
-    
+#ifdef DEBUG
     std::cout << intip_to_string(net_ip_1) << "(" << (net_ip_1.first >> (32 - net_ip_2.second))<< ") < ";
     std::cout << intip_to_string(net_ip_2) << "(" << (net_ip_2.first >> (32 - net_ip_2.second))<< ") = "; 
+#endif
     return (net_ip_1.first >> (32 - net_ip_2.second)) < (net_ip_2.first >> (32 - net_ip_2.second));
 }
 
-static void add_node(Node *root, Net_IP net_ip) {
-    std::bitset<32> ip_set(net_ip.first);
-    
-    Node **now_node = &root;
 
+Node *right_rotate(Node *now_node) {
+    Node *right_left_node = now_node->left->right;
+    Node *left_node       = now_node->left;
+    
+    left_node->right = now_node;
+    now_node->left   = right_left_node;
+
+    now_node->depth = std::max(now_node->left->depth, now_node->right->depth) + 1;
+    left_node->depth = std::max(left_node->left->depth, left_node->right->depth) + 1;
+
+    return left_node;
 }
 
+
+Node *left_rotate(Node *now_node) {
+    Node *right_node = now_node->right;
+    Node *right_left_node = now_node->right->left;
+
+    right_node->left = now_node;
+    now_node->right = right_left_node;
+
+    now_node->depth = std::max(now_node->left->depth, now_node->right->depth) + 1;
+    right_node->depth = std::max(right_node->left->depth, right_node->right->depth) + 1;
+
+    return right_node;
+}
+
+
+Node *big_right_rotate(Node *now_node) {
+    now_node->right = right_rotate(now_node->right);
+    
+    return left_rotate(now_node);
+}
+
+
+Node *big_left_rotate(Node *now_node) {
+    now_node->left = left_rotate(now_node->left);
+    
+    return right_rotate(now_node);
+}
+
+Net_IP get_net_ip(Node *now_node) {
+    return {now_node->net_ip, now_node->mask};
+}
+
+Node *rotate_node(Node *now_node, Net_IP net_ip) {
+    auto balance = now_node->left->depth - now_node->right->depth;
+
+    if (balance > 1  and prefix_compare(net_ip, get_net_ip(now_node->left)))
+        return left_rotate(now_node);
+    if (balance < -1 and prefix_compare(get_net_ip(now_node->right), net_ip))
+        return right_rotate(now_node);
+    if (balance > 1  and prefix_compare(get_net_ip(now_node->left), net_ip))
+        return big_left_rotate(now_node);
+    if (balance < -1 and prefix_compare(net_ip, get_net_ip(now_node->right)))
+        return big_right_rotate(now_node);
+
+    return now_node;
+}
+
+
+static Node* add_node(Node *root, Net_IP net_ip) {
+    if (root == nullptr)
+        return new Node(net_ip.second, net_ip.first, nullptr, nullptr);
+    if (prefix_compare(net_ip, get_net_ip(root)))
+        root->left = add_node(root->left, net_ip);
+    else 
+        root->right = add_node(root->right, net_ip);
+
+    root->depth = std::max(root->left->depth, root->right->depth) + 1;
+
+    return root;
+}
+
+
 Node *build_avl_tree(Table &table) {
-    Node *root = new Node();
+    Node *root = nullptr;
 
     for (auto now_entry : table) {
-        std::cout << intip_to_string(now_entry);
+        root = add_node(root, now_entry.first);
     }
 
     return root;
