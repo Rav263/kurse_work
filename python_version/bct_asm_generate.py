2dd6d04a6af95aeb946480536bfb0641fc8d322b
Никита Никифоров <nickiforov.nik@gmail.com>
2020-04-22 15:36:57 +0300

    Start writing asm generator

--- python_version/bct_asm_generate.py
+++ python_version/bct_asm_generate.py
@@ -14,4 +14,22 @@ def load_default_defenitions(file_name):
 
         defs[words[1]] = eval(words[1])
 
+    defs_file.close()
+
     return defs
+
+
+def load_table_args(file_name):
+    args = dict()
+
+    args_file = open(file_name, "r")
+
+    for line in args_file:
+        words = line.strip().split()
+
+        if len(words) == 0:
+            continue
+
+    return args
+
+
